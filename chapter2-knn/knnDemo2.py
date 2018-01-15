412259f4e937634f51be98c38c3407930e43ed7c
loulan123 <dongjingwei1996@gmail.com>
2018-01-15 13:36:20 +0800

    k-近邻算法(knn)

---
+++ chapter2-knn/knnDemo2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 10:57:20 2018
+
+@author: loulan
+"""
+
+import numpy as np
+
+# 利用k近邻算法改进约会网站的配对效果
+# 收集数据--》准备数据--》分析数据--》训练算法--》测试算法--》使用算法
+
+"""
+分为三种类型
+largeDoses  极具魅力的
+smallDoses  魅力一般的
+didntlike   不喜欢的
+"""
